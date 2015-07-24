8ae03568daaaccd04c47cb779c02c219dae760c5
MARIE <miss.amadis@gmail.com>
2015-07-24 11:36:33 +0200

    page admissions

--- template-home-program.php
+++ template-home-program.php
@@ -136,8 +136,8 @@ Template Name: Home Program
 
 		
         <?php if (have_posts()) : while (have_posts()) : the_post();?>
-<?php the_content(); ?>
-<?php endwhile; endif; ?>
+        <?php the_content(); ?>
+        <?php endwhile; endif; ?>
 </div>
     
    
